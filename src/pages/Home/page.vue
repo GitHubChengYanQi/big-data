edd67cfa9f474ba7aa0c3d2f10839c46b98c6b77
Genius <2683941980@qq.com>
2023-03-16 20:03:28 +0800

    修改传参

--- src/pages/Home/page.vue
+++ src/pages/Home/page.vue
@@ -1,29 +1,40 @@
 <template>
-  <view>
+	<view>
 
-  </view>
+	</view>
 </template>
 
 <script>
+	export default {
+		name: 'Home',
+		data() {
+			return {
+				embed: {},
+			}
+		},
+		mounted() {
+		
+			const embed = plus.webview.create('http://192.168.31.111:8000/#/stock');
+	
+			embed.addEventListener('loaded', function() {
+				
+				// const token = getApp().globalData.token
+			
+				// const baseUrl = getApp().globalData.baseUrl
+				
+				// embed.evalJS(`window.token=${JSON.stringify(getApp().globalData)}`);
+				
+				embed.evalJS(`window.globalData=${JSON.stringify(getApp().globalData)}`);
+				
+				// embed.evalJS(`window.init(${JSON.stringify(getApp().globalData)})`);
+				
+				embed.show();
+				
+			}, false);
 
-export default {
-  name: 'Home',
-  data() {
-    return {
-      embed: {},
-    }
-  },
-  mounted() {
-	  console.log(getApp().globalData.baseUrl)
-    const embed = plus.webview.create('https://wx.zz2025.com/data/#/stock', 'stock', {}, {
-      token: getApp().globalData.token,
-      baseURI: getApp().globalData.baseUrl,
-    });
-    this.embed = embed
-    embed.show();
 
 
-  },
-  methods: {}
-}
+		},
+		methods: {}
+	}
 </script>
